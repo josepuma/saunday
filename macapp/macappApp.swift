79f71fb7d4b790b28d5ba30997b012ab318bec9f
José Puma <45923235+josepuma@users.noreply.github.com>
2026-04-23 19:43:04 -0400

    Initial Commit

---
+++ macapp/macappApp.swift
@@ -0,0 +1,17 @@
+//
+//  macappApp.swift
+//  macapp
+//
+//  Created by José Puma on 23-04-26.
+//
+
+import SwiftUI
+
+@main
+struct macappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macappTests/macappTests.swift
@@ -0,0 +1,17 @@
+//
+//  macappTests.swift
+//  macappTests
+//
+//  Created by José Puma on 23-04-26.
+//
+
+import Testing
+@testable import macapp
+
+struct macappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
